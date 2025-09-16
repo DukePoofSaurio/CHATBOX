38deb873dd50856d757c039343a4a4e5e0200966
DukePoofSaurio <zanty423@gmail.com>
2025-09-15 19:36:16 -0500

    modified:   main.py 	new file:   templates/index.html

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/index")
 def index():
     return "Hola Mundo"
+
+@app.route("/index")
+def index():
+    return render_template('index.html')
 
 app.run(host='0.0.0.0', port=81)
