51e6a4fb9dcd2c9501a43739646dcc5b1abfe4d1
DukePoofSaurio <zanty423@gmail.com>
2025-09-16 19:43:47 -0500

    Clase Martes

---
+++ Documents/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/home")
+def home():
+    return "Hola Mundo"
+
+@app.route("/index")
+def index():
+    return render_template('index.html')
+
+app.run(host='0.0.0.0', port=81, debug=True)
