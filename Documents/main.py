a7ea0b912fa1657bbf8b92e96930b268ada0db77
DukePoofSaurio <zanty423@gmail.com>
2025-09-16 19:51:50 -0500

    Clase Martes

--- Documents/main.py
+++ Documents/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -10,4 +10,20 @@ def home():
 def index():
     return render_template('index.html')
 
-app.run(host='0.0.0.0', port=81, debug=True)
+@app.route("/base")
+def base():
+    return render_template('base.html')
+
+@app.route("/mision")
+def mision():
+    return render_template('mision.html')
+
+@app.route("/vision")
+def vision():
+    return render_template('vision.html')
+
+@app.route("/programas")
+def programas():
+    return render_template('programas.html')
+
+app.run(host='0.0.0.0', port=5000, debug=True)
